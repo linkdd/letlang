a90728c2b26dabda9c11693f3ca7326178a1fd9d
David Delassus <david.jose.delassus@gmail.com>
2022-05-15 01:16:56 +0200

    :sparkles: compile only perform debug side effect

--- letlang_compiler/codegen/function.py
+++ letlang_compiler/codegen/function.py
@@ -1,3 +1,6 @@
+import json
+
+
 class FunctionMixin:
     def walk_FunctionCall(self, node, scope):
         func_path = node["data"]["func_name"].split(".")
@@ -21,3 +24,18 @@ class FunctionMixin:
             raise NameError(f"Function {func_name} not found in scope")
 
         assert False
+
+    def walk_EffectCall(self, node, scope):
+        if node["data"]["effect_name"] != "debug":
+            raise NotImplementedError("only debug side effect is supported for now")
+
+        call_params = [
+            self.walk(call_param, scope=scope)
+            for call_param in node["data"]["call_params"]
+        ]
+
+        template = self.get_template("trigger_side_effect.rs.j2")
+        return template.render(
+            effect_name=json.dumps(node["data"]["effect_name"]),
+            call_params=call_params,
+        )

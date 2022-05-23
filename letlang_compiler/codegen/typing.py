c302f03fbb0a9c63c86c1ed98a49a51585718230
David Delassus <david.jose.delassus@gmail.com>
2022-05-23 11:37:03 +0200

    :sparkles: generate code for TypeName node

--- letlang_compiler/codegen/typing.py
+++ letlang_compiler/codegen/typing.py
@@ -1,4 +1,42 @@
+BUILTIN_TYPES = [
+    "bool",
+    "int",
+    "number",
+    "string",
+    "atom",
+]
+
+
 class TypingMixin:
     def walk_ValueType(self, node, scope):
         primitive_code = self.walk(node["data"], scope=scope)
         return f"types::ValueType {{ llval: {primitive_code} }}"
+
+    def walk_TypeName(self, node, scope):
+        type_name = node["data"]["name"]
+
+        if type_name in scope:
+            item_type, sub_scope = scope[type_name]
+
+            if item_type == "type":
+                raise NotImplementedError("Custom class not supported yet")
+
+        elif type_name in BUILTIN_TYPES:
+            match type_name:
+                case "bool":
+                    return "types::BooleanType {}"
+
+                case "int":
+                    return "types::IntegerType {}"
+
+                case "number":
+                    return "types::NumberType {}"
+
+                case "string":
+                    return "types::StringType {}"
+
+                case "atom":
+                    return "types::AtomType {}"
+
+        else:
+            raise NameError(f"Type {type_name} not found in scope")

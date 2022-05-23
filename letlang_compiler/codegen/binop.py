2beb97129b8b6c5be8245cada79f804702638565
David Delassus <david.jose.delassus@gmail.com>
2022-05-23 11:38:10 +0200

    :sparkles: add codegen for BinaryOperation node

---
+++ letlang_compiler/codegen/binop.py
@@ -0,0 +1,23 @@
+class BinaryOperationMixin:
+    def walk_BinaryOperation(self, node, scope):
+        lhs = self.walk(node["data"]["lhs"], scope=scope)
+        rhs = self.walk(node["data"]["rhs"], scope=scope)
+        op = node["data"]["op"]
+
+        match op:
+            case "+":
+                fn_op = "operations::add"
+
+            case "-":
+                fn_op = "operations::sub"
+
+            case op:
+                raise NotImplementedError(f"Operator {op} not yet supported")
+
+        template = self.get_template("binop_expression.rs.j2")
+        return template.render(
+            lhs=lhs,
+            rhs=rhs,
+            op=op,
+            fn_op=fn_op,
+        )

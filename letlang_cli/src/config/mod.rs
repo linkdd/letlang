f1308b526f3d23152d87c19a696ff1349e826659
David Delassus <david.jose.delassus@gmail.com>
2022-08-13 21:13:06 +0200

    :sparkles: add cli project

---
+++ letlang_cli/src/config/mod.rs
@@ -0,0 +1,39 @@
+use serde::Deserialize;
+
+
+#[derive(Deserialize, Debug)]
+pub struct ProjectConfig {
+  pub package: PackageConfig,
+  pub executable: ExecutableConfig,
+  pub toolchain: ToolchainConfig,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct PackageConfig {
+  pub name: String,
+  pub version: String,
+  pub description: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ExecutableConfig {
+  pub bin: String,
+  pub module: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ToolchainConfig {
+  pub letlang: String,
+  pub rust: String,
+}
+
+impl ProjectConfig {
+  pub fn load<P>(filename: P) -> Result<ProjectConfig, Box<dyn std::error::Error>>
+    where P: AsRef<std::path::Path>
+  {
+    let content = std::fs::read_to_string(filename)?;
+    let config: ProjectConfig = toml::from_str(&content)?;
+
+    Ok(config)
+  }
+}
---
+++ letlang_cli/src/main.rs
@@ -0,0 +1,106 @@
+use letlang_cli::config::ProjectConfig;
+use letlang_compiler::Compiler;
+
+use clap::{Parser, Subcommand};
+use std::path::{Path, PathBuf};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+#[clap(propagate_version=true)]
+struct Cli {
+  #[clap(short, long, value_parser)]
+  project_path: Option<PathBuf>,
+  #[clap(subcommand)]
+  command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+  Build,
+  Run,
+}
+
+fn main() -> Result<()> {
+  let cli = Cli::parse();
+
+  let project_dir = std::fs::canonicalize(
+    cli.project_path.unwrap_or(PathBuf::from("."))
+  )?;
+  let project_config = load_config(&project_dir)?;
+
+  match cli.command {
+    Commands::Build => {
+      build_project(&project_dir, &project_config)?;
+    },
+    Commands::Run => {
+      build_project(&project_dir, &project_config)?;
+      run_project(&project_config)?;
+    }
+  }
+
+  Ok(())
+}
+
+fn load_config<P: AsRef<Path>>(project_dir: P) -> Result<ProjectConfig>
+{
+  let mut project_path = PathBuf::new();
+  project_path.push(project_dir);
+  project_path.push("letproject.toml");
+  ProjectConfig::load(project_path)
+}
+
+fn build_project<P: AsRef<Path>>(project_dir: P, cfg: &ProjectConfig) -> Result<()>
+{
+  let inputs = glob_sources(&project_dir);
+  let target_dir = make_target_dir(&project_dir)?;
+
+  let compiler = Compiler::new();
+  compiler.compile(inputs, target_dir)?;
+
+  Ok(())
+}
+
+fn run_project(cfg: &ProjectConfig) -> Result<()> {
+  println!("run...");
+  Ok(())
+}
+
+fn glob_sources<P: AsRef<Path>>(project_dir: P) -> Vec<PathBuf>
+{
+  let mut source_dir = PathBuf::new();
+  source_dir.push(project_dir);
+  source_dir.push("src");
+
+  let paths: Vec<PathBuf> = walkdir::WalkDir::new(source_dir)
+    .into_iter()
+    .filter_map(|e| e.ok())
+    .filter(|e| e.file_type().is_file())
+    .filter(|e| {
+      if let Some(ext) = e.path().extension() {
+        match ext.to_str() {
+          Some("let") => true,
+          _ => false
+        }
+      }
+      else {
+        false
+      }
+    })
+    .map(|e| e.path().to_path_buf())
+    .collect();
+
+  paths
+}
+
+fn make_target_dir<P: AsRef<Path>>(project_dir: P) -> Result<PathBuf> {
+  let mut target_dir = PathBuf::new();
+  target_dir.push(project_dir);
+  target_dir.push(".llbuild");
+
+  std::fs::create_dir_all(&target_dir)?;
+
+  Ok(target_dir)
+}

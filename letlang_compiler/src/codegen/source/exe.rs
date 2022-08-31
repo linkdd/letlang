f05f12584714fbbd34b87586a61f9cc2ba6a5e91
David Delassus <david.jose.delassus@gmail.com>
2022-08-31 22:52:01 +0200

    :recycle: create atom table in executable crate

--- letlang_compiler/src/codegen/source/exe.rs
+++ letlang_compiler/src/codegen/source/exe.rs
@@ -9,33 +9,31 @@ use askama::Template;
 #[template(path = "executable.rs.j2")]
 struct ExecutableTemplate {
   pub crate_name: String,
-  pub builtin_atoms: Vec<BuiltinAtomEntry>,
+  pub atoms: Vec<AtomTemplate>,
 }
 
-struct BuiltinAtomEntry {
-  string: String,
+struct AtomTemplate {
+  repr: String,
   symbol: usize,
 }
 
 
 impl<'compiler> Generator<'compiler> {
   pub fn gen_exe_source(&mut self, crate_name: &str) -> CompilationResult<String> {
-    let builtin_atoms = vec![
-      "@ok",
-      "@error",
-      "@type_error",
-      "@normal"
-    ]
-      .into_iter()
-      .map(|a| BuiltinAtomEntry {
-        string: a.to_string(),
-        symbol: self.atom_interner.get_or_intern(a).to_usize(),
+    for builtin_atom in vec!["@ok", "@error", "@type_error", "@normal"] {
+      self.atom_interner.get_or_intern(builtin_atom);
+    }
+
+    let atoms = self.atom_interner.into_iter()
+      .map(|(symbol, repr)| AtomTemplate {
+        repr: repr.to_string(),
+        symbol: symbol.to_usize(),
       })
       .collect();
 
     let context = ExecutableTemplate {
       crate_name: crate_name.to_string(),
-      builtin_atoms,
+      atoms,
     };
 
     let source_code = context.render().map_err(|e| {

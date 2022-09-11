f88803227fc86b7d11ed4f197202b105652c3408
David Delassus <david.jose.delassus@gmail.com>
2022-09-11 23:52:59 +0200

    :sparkles: add assert_defined helper to runtime

--- sources/letlang_runtime/src/api/helpers/mod.rs
+++ sources/letlang_runtime/src/api/helpers/mod.rs
@@ -2,10 +2,12 @@ mod type_check;
 mod pattern;
 mod function;
 mod params;
+mod variable;
 
 pub use self::{
   type_check::assert_type,
   pattern::assert_match,
   params::assert_param_count,
   function::call_function,
+  variable::assert_defined,
 };
---
+++ sources/letlang_runtime/src/api/helpers/variable.rs
@@ -0,0 +1,23 @@
+use crate::api::*;
+use crate::builtins::prelude::*;
+
+
+pub async fn assert_defined<'scope>(
+  co: &FunctionCoroutine,
+  context: Arc<Mutex<TaskContext>>,
+  locals: &mut Locals<'scope>,
+  symbol: &'static str,
+) -> Value {
+  match locals.lookup_symbol(symbol) {
+    None => {
+      let err = OperationError::Undefined(symbol.to_string());
+      let exc = err.to_letlang_value(context.clone()).await;
+
+      co.yield_(FunctionInterruption::Exception(exc)).await;
+      unreachable!();
+    },
+    Some(val) => {
+      val.clone()
+    }
+  }
+}

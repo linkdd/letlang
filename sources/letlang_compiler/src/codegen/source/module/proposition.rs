17e48888e44bcd6be5662fcf47f81b5bb6923a68
David Delassus <david.jose.delassus@gmail.com>
2022-09-11 19:04:44 +0200

    :sparkles: codegen for constraint proposition

--- sources/letlang_compiler/src/codegen/source/module/proposition.rs
+++ sources/letlang_compiler/src/codegen/source/module/proposition.rs
@@ -6,12 +6,44 @@ use letlang_ast::{
   statement::*,
 };
 
+use askama::Template;
+
+
+#[derive(Template)]
+#[template(path = "statement/proposition_constraint.rs.j2", escape = "none")]
+struct ConstraintTemplate {
+  symbol: String,
+  type_code: String,
+  checks: Vec<String>,
+}
+
 
 impl<'compiler> Generator<'compiler> {
   pub fn gen_proposition(&self, node: &Node<Proposition>) -> CompilationResult<String> {
     match node.data.as_ref() {
       Proposition::Constraint(constraint) => {
-        todo!();
+        let type_code = self.gen_typeref(&constraint.symbol_type)?;
+        let mut checks = vec![];
+
+        for check_node in constraint.checks.iter() {
+          let check_code = self.gen_expression(check_node)?;
+          checks.push(check_code);
+        }
+
+        let context = ConstraintTemplate {
+          symbol: constraint.symbol_name.clone(),
+          type_code,
+          checks,
+        };
+
+        let source_code = context.render().map_err(|e| {
+          CompilationError::new_located(
+            &node.location,
+            format!("Could not generate constraint source: {}", e),
+          )
+        })?;
+
+        Ok(source_code)
       },
       Proposition::Evaluation(expr) => {
         self.gen_expression(expr)
